d5cdcecec1e60722b9eaba3d07fb8de100d42095
rania ben amor <48763401+raniabenamor@users.noreply.github.com>
2021-08-05 11:50:30 +0200

    Add files via upload

---
+++ InlineDataAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UnitTestCurrencyConverter
+{
+    internal class InlineDataAttribute : Attribute
+    {
+        private double v1;
+        private string v2;
+
+        public InlineDataAttribute(double v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace UnitTestCurrencyConverter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+        [TestMethod]
+        public void TestClearControls()
+        {
+            var test = new UnitTest1();
+
+            
+        }
+        
+        
+        // Function to test for Positive Integers with zero inclusive  
+        public bool IsWholeNumber(String strNumber)
+        {
+            Regex objNotWholePattern = new Regex("[^0-9]");
+            return !objNotWholePattern.IsMatch(strNumber);
+        }
+
+	}
+}
